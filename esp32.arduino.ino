ab8a7b5ce891147d17f561994ae7397e8f246240
Navindu <navindudesilva55@gmail.com>
2024-02-06 22:58:03 +0530

    updated for dht22 sensor

--- esp32.arduino.ino
+++ esp32.arduino.ino
@@ -6,5 +6,14 @@ DHTesp dhtSensor;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("vasduya");
+  dhtSensor.setup(DHT_PIN , DHTesp::DHT22);
+
+}
+
+void loop() {
+  TempAndHumidity data = dhtSensor.getTempAndHumidity();
+  Serial.println("Temp: " + String(data.temperature,2) + "C") ;
+  Serial.println("Humidity: "+ String(data.humidity,1) + "%");
+  Serial.println("---") ;
+  delay(1000);
 }
